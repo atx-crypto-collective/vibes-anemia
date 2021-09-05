487ba261c2aec9e7f8ac47c46207721cbfa60e7f
Lisa Ma <lisa@yonomi.co>
2021-09-04 23:52:35 -0500

    structure work

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
+// define anemia requirement
+// traverse pieces on SSW
+// boop out data to a JSON file for posterity
+
+// stretch: display nicely on a website
+// stretch: listen for events on SSW contract to update website
+
 (async () => {
   
 })();
